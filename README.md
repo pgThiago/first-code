16aa7547fb0fb27dd1820f80ede0bce2b2f1e25b
Thiago Silva <thiago25pro@gmail.com>
2020-10-31 20:18:54 -0300

    first commit of one of my first codes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# first-code
+## One of my first codes.
+## Dated: Thursday, August 16, 2018, 10:54:20 PM.
---
+++ aprendendo.py
@@ -0,0 +1,5 @@
+day = input ('day=')
+month = input ('month=')
+year = input ('year=')
+print ('você nasceu no dia', day, 'de', month, 'de', year)
+
